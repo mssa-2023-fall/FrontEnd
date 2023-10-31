8e73f7cb1ae5d08f0a44a88d6b12d3f5344d4d11
Alejandro Gonzalez <alejandro.gonzalezde@outlook.com>
2023-10-31 09:56:11 -0400

    Added cookie button to index page

---
+++ MortgageConsole/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MortgageCalculatorLibrary;
+using Spectre;
+using Spectre.Console;
+using System;
+using System.Numerics;
+
+Console.WriteLine("Hello, Home Buyer!");
+
+Console.WriteLine("1. Build a new mortgage: ");
+Console.WriteLine("2. View existing mortgage(s): ");
+Console.WriteLine("3. Check federal rates. This will take you outside of the current app(development)"); 
+Console.WriteLine("4. About Us: ");
+Console.Write("Your selection: ");
+var selection = int.Parse(Console.ReadLine());
+
+
+
+
+Console.WriteLine("Enter the corresponding amounts below: ");
+        Console.Write("Principal Amount: ");
+        decimal principal = decimal.Parse(Console.ReadLine());
+        Console.Write("Interest: ");
+        decimal interest = decimal.Parse(Console.ReadLine());
+        Console.Write("First Payment Date: ");
+        DateTime firstPayment = DateTime.Parse(Console.ReadLine());
+        Console.Write("Loan Term in years: ");
+        int term = int.Parse(Console.ReadLine());
+        Console.Write("Monthly escrow: ");
+        decimal escrow = decimal.Parse(Console.ReadLine());
+        
+        Mortgage mortgage = new Mortgage(interest, principal, firstPayment, term, escrow);
+
+
+Console.WriteLine(mortgage.CalculateMonthlyPayment());
+
---
+++ TestMiddleware/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+//
+builder.Services.AddDistributedMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+//i think we need to check if we close the page and then comeback to it. but we are in dev mode
+//im going to push this to the repo
+//store values in cookie upon hitting calculate
+//look for and retrieve values when page reloaded
+
